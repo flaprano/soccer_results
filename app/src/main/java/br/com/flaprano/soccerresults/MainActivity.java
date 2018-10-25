b0fd8dc53b3040b9661a898876391410af32d0c7
Felipe Laprano <flaprano@gmail.com>
2018-10-24 23:31:25 -0300

    create interface

--- app/src/main/java/br/com/flaprano/soccerresults/MainActivity.java
+++ app/src/main/java/br/com/flaprano/soccerresults/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.flaprano.soccerresults;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,127 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setBtnActions();
+    }
+
+    private void setBtnActions() {
+
+    }
+
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+
+    private void setBtnIncrementCornerKicksTeamB() {
+        Button btnIncrementCornerKicksTeamB = (Button) findViewById(R.id.btnIncrementCornerKicksTeamB);
+        btnIncrementCornerKicksTeamB.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamB = (TextView) findViewById(R.id.txtCornerKicksCounterTeamB);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamB.getText().toString()) + 1;
+                txtCornerKicksCounterTeamB.setText(Integer.toString(counter));
+            }
+        });
+    }
+
+    private void setBtnIncrementFaultsTeamA() {
+        Button btnIncrementFaultsTeamA = (Button) findViewById(R.id.btnIncrementFaultsTeamA);
+        btnIncrementFaultsTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtFaultsCounterTeamA = (TextView) findViewById(R.id.txtFaultsCounterTeamA);
+                int counter = Integer.parseInt(txtFaultsCounterTeamA.getText().toString()) + 1;
+                txtFaultsCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+
+    private void setBtnIncrementFaultsTeamB() {
+        Button btnIncrementFaultsTeamB = (Button) findViewById(R.id.btnIncrementFaultsTeamB);
+        btnIncrementFaultsTeamB.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtFaultsCounterTeamB = (TextView) findViewById(R.id.txtFaultsCounterTeamB);
+                int counter = Integer.parseInt(txtFaultsCounterTeamB.getText().toString()) + 1;
+                txtFaultsCounterTeamB.setText(Integer.toString(counter));
+            }
+        });
+    }
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
+    }
+
+    private void setBtnIncrementCornerKicksTeamA() {
+        Button btnIncrementCornerKicksTeamA = (Button) findViewById(R.id.btnIncrementCornerKicksTeamA);
+        btnIncrementCornerKicksTeamA.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                TextView txtCornerKicksCounterTeamA = (TextView) findViewById(R.id.txtCornerKicksCounterTeamA);
+                int counter = Integer.parseInt(txtCornerKicksCounterTeamA.getText().toString()) + 1;
+                txtCornerKicksCounterTeamA.setText(Integer.toString(counter));
+            }
+        });
     }
 }
